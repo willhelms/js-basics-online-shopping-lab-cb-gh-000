fb7dd28b02173ceb5fc83b3d0b1fca096fde193d
william <william060@student.itb.ac.id>
2018-07-09 08:19:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ function setCart(c) {
 function addToCart(item) {
  // write your code here
  var items = {};
-// var price = Math.floor(Math.random() * 10) + 1;
- items = items.assign(items, { "itemName : ${item}, itemPrice: ${price}"});
+ var price = Math.floor(Math.random() * 10) + 1;
+ items = items.assign(items, { "itemName : ${item}, itemPrice: ${price}"})
  return items;
 }
 
