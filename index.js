5edcad90a9a67f6abe189d2c9db8016b761ed0f9
william <william060@student.itb.ac.id>
2018-07-09 08:32:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,9 +14,9 @@ function addToCart(item) {
  var items = {};
  //random number 1 - 100
  var price = Math.floor(Math.random() * 99) + 1;
- console.log(price);
- //Object.assign(items, { 'itemName : ${item}, itemPrice: ${price}'})
- //cart.push(items);
+ 
+ Object.assign(items, 'itemName : ${item}', 'itemPrice: ${price}'})
+ cart.push(items);
 }
 
 function viewCart() {
